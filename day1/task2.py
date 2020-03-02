6798661f9ef5dde77586ece962977e384021400d
hiroakitanaka <hiroaki-tanaka216@keio.jp>
2020-03-02 10:53:21 +0900

    task

---
+++ day1/task2.py
@@ -0,0 +1,11 @@
+for(int i=1; i<=30; i++){
+    if(i%15==0){
+         print("FizzBuzz\n")
+} elif(i%3==0 && i%15!=0){
+         print("Fizz\n")
+} elif(i%5==0 && i%15!=0){
+         print("Buzz\n")
+} else{
+      print(i)
+}
+}
