98502ced2aa8353d48cacc569308dfd2c5e221dc
hiroakitanaka <hiroaki-tanaka216@keio.jp>
2020-03-10 15:16:20 +0900

    day2

---
+++ day2/task2_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+
+x = np.array([1.0, 0.5])
+W = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+b = np.array([0.1, 0.2, 0.3])
+
+t = np.dot(W.T, x) + b
+
+
+def relu(tmp):
+    return np.maximum(0, tmp)
+
+
+print(relu(t))
