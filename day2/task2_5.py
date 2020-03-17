443d9d6eaccf39cb0cc2e58b63284f3d7f8e2960
hiroakitanaka <hiroaki-tanaka216@keio.jp>
2020-03-17 11:56:31 +0900

    day2

--- day2/task2_5.py
+++ day2/task2_5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from day2.task2_3 import SingleLayer
 
 # a = np.array([0.3, 0.9, 4.0])
 
@@ -7,4 +8,38 @@ def softmax(x):
     sum_exp_x = np.sum(exp_x)
     return exp_x / sum_exp_x
 
-print(softmax(a))
+class SoftmaxLayer:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+    def forward(self, t):
+        # tmp = np.dot(t, self.W) + self.b
+        # return softmax(tmp)
+        return softmax(t)
+
+class MLP_3Layer:
+    def __init__(self, w1, w2, w3, b1, b2, b3):
+        self.single_layer1 = SingleLayer(w1, b1)
+        self.single_layer2 = SingleLayer(w2, b2)
+        self.single_layer3 = SoftmaxLayer(w3, b3)
+
+    def forward(self, x):
+        tmp1 = self.single_layer1.forward(x)
+        tmp2 = self.single_layer2.forward(tmp1)
+        tmp3 = self.single_layer3.forward(tmp2)
+        return tmp3
+
+
+aMLP_3Layer = MLP_3Layer(np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
+                         np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
+                         np.array([[0.1, 0.3], [0.2, 0.4]]),
+                         np.array([0.1, 0.2, 0.3]),
+                         np.array([0.1, 0.2]),
+                         np.array([0.1, 0.2]))
+print(aMLP_3Layer.forward(np.array([[1.0, 0.5], [-0.3, -0.2], [0.0, 0.8], [0.3, -0.4]])))
+
+
+
+
+
